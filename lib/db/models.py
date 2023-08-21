1d21eabaf18412766a54893444099344a77ae527
Franklin Hau <fhauchi@gmail.com>
2023-08-21 05:27:56 -0600

    comments on Import statements

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,2 +1,8 @@
+# creating new database connections.
+# defining columns in a database table and their types.
 from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey 
+# to create new Session objects, to interact with the database.
+# relationships between tables (like foreign keys).
 from sqlalchemychemy.orm import sessionmaker, relationship
+# used to create a new base class for declarative models.
+from aqlalchemychemy.ext.declarative import declarative_base
