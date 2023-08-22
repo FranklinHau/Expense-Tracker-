e8d8fbe79d5ae6d70f35a8d57e2c2a3ebf65532d
Franklin Hau <fhauchi@gmail.com>
2023-08-22 06:00:34 -0600

    def main() created

--- lib/main.py
+++ lib/main.py
@@ -1,2 +1,11 @@
 from models import session, User, Expense 
 from utilities import hash_password, check_password
+
+def main():
+    while True:
+        print('1. Register')
+        print('2. Login')
+        print('3. Exit')
+        choice = input('Enter your choice: ')
+
+        
