838b9f2be6e089e175876a7cb357bef0eaf2984f
Franklin Hau <fhauchi@gmail.com>
2023-08-22 07:35:08 -0600

    fix bug in models.py

--- lib/db/models.py
+++ lib/db/models.py
@@ -3,7 +3,7 @@
 from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey 
 # to create new Session objects, to interact with the database.
 # relationships between tables (like foreign keys).
-from sqlalchemychemy.orm import sessionmaker, relationship
+from sqlalchemy.orm import sessionmaker, relationship
 # used to create a new base class for declarative models.
 from aqlalchemychemy.ext.declarative import declarative_base
 
